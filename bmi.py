fc85e3307cf384ca0a605372cf63ea9a16e58c19
sumeetby16 <sumeetby16>
2025-10-23 17:20:02 +0530

    salary

---
+++ bmi.py
@@ -0,0 +1,8 @@
+weight = float(input("Enter weight (in kg): "))
+height = float(input("Enter height (in meters): "))
+
+bmi = weight / (height ** 2)
+
+print("Weight:", weight, "kg")
+print("Height:", height, "m")
+print("BMI:", bmi)
